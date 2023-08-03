06395198956a0c2daf158131f5246dbde3668b63
Guilherme Cardozo <cardozoguilherme18@gmail.com>
2023-08-03 16:23:04 -0300

    funções de centralizar tela, gerando pdf e salvando no destino

--- gerador_qrcode.py
+++ gerador_qrcode.py
@@ -1,23 +1,59 @@
 import qrcode
 from tkinter import * 
-from tkinter.filedialog import askdirectory
+from tkinter.filedialog import askdirectory,asksaveasfile
+from tkinter.messagebox import showerror,showinfo
+
+def center(win):
+    # :param win: the main window or Toplevel window to center
+
+    # Apparently a common hack to get the window size. Temporarily hide the
+    # window to avoid update_idletasks() drawing the window in the wrong
+    # position.
+    win.update_idletasks()  # Update "requested size" from geometry manager
+
+    # define window dimensions width and height
+    width = win.winfo_width()
+    frm_width = win.winfo_rootx() - win.winfo_x()
+    win_width = width + 2 * frm_width
+
+    height = win.winfo_height()
+    titlebar_height = win.winfo_rooty() - win.winfo_y()
+    win_height = height + titlebar_height + frm_width
+
+    # Get the window position from the top dynamically as well as position from left or right as follows
+    x = win.winfo_screenwidth() // 2 - win_width // 2
+    y = win.winfo_screenheight() // 2 - win_height // 2
+
+    # this is the line that will center your window
+    win.geometry('{}x{}+{}+{}'.format(width, height, x, y))
+
+    # This seems to draw the window frame immediately, so only call deiconify()
+    # after setting correct window position
+    win.deiconify()
+
+
 
 
 def directory():
     # diretorio selecionado pelo usuario
-    global filepath 
-    filepath= askdirectory(title="Salvar QRCode")
-    entradaCaminho = Label(janela,width=55,text=filepath,bg="#ECECEC")
-    entradaCaminho.grid(column=1, row=2, padx=0, pady=5)
+    filepath= asksaveasfile(mode="w",title="",defaultextension=".jpg")
     return filepath
 
 def criarQR():
-
+    
     nomeArquivo = nome.get()
-    if (nomeArquivo=="" ||nomeArq):
-        print('uauuu')
-    codigo = qrcode.make(link.get())
-    codigo.save("teste.jpg")
+    
+
+    if (nomeArquivo=="" or nomeArquivo ==" "):
+        showerror("ERRO","Insira um nome válido no nome do arquivo")
+    else:
+        nomeArquivo +=".jpg"
+        rota = directory()
+        codigo = qrcode.make(link.get())
+        if(codigo.save(rota+"/"+nomeArquivo)==True):
+            showinfo("QR Code","QR Code criado com sucesso!")
+
+
 
 texto= str
 
@@ -26,6 +62,7 @@ janela.title("Validar resposta")
 janela.geometry("640x320")
 janela.resizable(0,0)
 janela.configure(bg="#ECECEC")
+center(janela)
 
 #LINK
 #   label
@@ -41,11 +78,11 @@ entradaLink.grid(column=1, row=0, padx=5, pady=5)
 #NOME DO ARQUIVO
 #   label
 labelNome = Label(janela, text="Nome do arquivo",bg="#ECECEC",fg="black",font=("7"))
-labelNome.grid(column=0, row=1, padx=0, pady=5)
+labelNome.grid(column=0, row=1, padx=5, pady=5)
 #   input
 nome =StringVar()
 entradaNome = Entry(janela,textvariable=nome,width=55)
-entradaNome.grid(column=1, row=1, padx=0, pady=5)
+entradaNome.grid(column=1, row=1, padx=5, pady=5)
 
 #LOCAL A SER SALVO
 #   label
